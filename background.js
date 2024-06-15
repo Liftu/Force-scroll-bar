08190e94a7046294187debbc2121ec0fffe2c15e
Liftu <louilou008@gmail.com>
2024-06-15 13:56:37 +0200

    Added background.js

---
+++ background.js
@@ -0,0 +1,24 @@
+async function ForceShowScrollBar(tab) {
+    try {
+        await browser.scripting.executeScript({
+            target: { tabId: tab.id },
+            func: () => {
+                let forced = false;
+                if (document.body.style.overflow == "hidden") {
+                    document.body.style.overflow = "";
+                    forced = true;
+                }
+                if (document.documentElement.style.overflow == "hidden") {
+                    document.body.style.overflow = "";
+                    forced = true;
+                }
+                if (forced)
+                    console.log("Scroll bar restored")
+            }
+        });
+    } catch (err) {
+        console.error(`failed to retore scroll bar: ${err}`);
+    }
+}
+
+browser.browserAction.onClicked.addListener(ForceShowScrollBar);
